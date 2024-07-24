996319f40817ff881418c6364b59b8fc5b9f7d4f
RubenNeo <ruben.lazaronp@gmail.com>
2024-07-24 12:46:28 +0200

    Aplication IMC Donde

--- ImcApp-IOS/HomeViewController.swift
+++ ImcApp-IOS/HomeViewController.swift
@@ -13,13 +13,21 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
+    @IBOutlet weak var heightStepper: UIStepper!
+    
+    @IBOutlet weak var Results: UILabel!
+    @IBOutlet weak var messageWeight: UILabel!
+    
     var height : Int = 160
     var weight : Float = 70
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        heightLabel.text = "\(height)cm"
+        weightLabel.text = String(format: "%.1fkg", weight)
+    
+       
     }
 
     @IBAction func onHeightChanged(_ sender: UIStepper) {
@@ -29,8 +37,46 @@ class HomeViewController: UIViewController {
     @IBAction func onWeightChanged(_ sender: UISlider) {
         weight = sender.value
         weightLabel.text = String(format: "%.1fkg", weight)
+        
     }
-
+    @IBAction func calculateBottom(_ sender: Any) {
+        updateIMC()
+    
     }
+    
+    func updateIMC() {
+           let heightInMeters = Float(height) / 100.0
+           let imc = weight / (heightInMeters * heightInMeters)
+           Results.text = String(format: "Results: %.2f", imc)
+        
+        
+        var message = ""
+        var color = UIColor.black
+        switch imc {
+        case 0..<18:
+            message = "Bajo de Peso"
+            color = UIColor.blue
+        case 18..<25:
+            message = "Peso Normal"
+            color = UIColor.green
+        case 25..<30:
+            message = "Sobrepeso"
+            color = UIColor.orange
+        case 30..<35:
+            message = "Obesidad Ligera"
+            color = UIColor.yellow
+        case 35..<39:
+            message = "Obesidad"
+            color = UIColor.red
+        default:
+            message = "Haz ejercicio"
+            color = UIColor.brown
+        
+        }
+        Results.textColor = color
+        messageWeight.text = message
+        messageWeight.textColor = color
+    }
+}
     
 
